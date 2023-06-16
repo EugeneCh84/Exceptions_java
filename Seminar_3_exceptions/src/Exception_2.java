9745f80048d60b3adc9cbad36266e14674fcfc87
EugeneCh84 <eugene.cherepanov.84@gmail.com>
2023-06-16 13:36:03 +0500

    done

---
+++ Seminar_3_exceptions/src/Exception_2.java
@@ -0,0 +1,17 @@
+public class Exception_2 {
+    public static void main(String[] args) {
+        int[] intArray = new int[]{2, 4, 8, 1, 5, 7};
+        int d = 0;
+        int index = 8;
+        if (d == 0) {
+            System.out.println("Denominator can not be zero.");
+        } else if (intArray == null) {
+            System.out.println("Array is null.");
+        } else if (index < 0 || index >= intArray.length) {
+            System.out.println("Index out of range.");
+        } else {
+            double catchedRes1 = (double) intArray[index] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        }
+    }
+}
